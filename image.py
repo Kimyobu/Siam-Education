cc14f8c89cf6fb0c6d1a30fe06d13fb355e848a3
Kimyobu <91616822+Kimyobu@users.noreply.github.com>
2023-09-21 12:59:45 +0700

    test with display image

---
+++ image.py
@@ -0,0 +1,33 @@
+import os
+import matplotlib.pyplot as plt
+from PIL import Image
+from datetime import datetime
+
+def display_images_sorted(folder_path, num_cols=4, image_width=4):
+    # อ่านรายชื่อไฟล์รูปภาพและเรียงลำดับตามวันที่และเวลา
+    image_filenames = sorted([f for f in os.listdir(folder_path) if f.endswith(('.jpg', '.jpeg', '.png', '.gif', '.bmp'))], 
+                             key=lambda x: os.path.getctime(os.path.join(folder_path, x)), reverse=True)
+
+    num_images = len(image_filenames)
+    num_rows = (num_images + num_cols - 1) // num_cols
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=(image_width * num_cols, image_width * num_rows))
+    axes = axes.flatten()
+
+    for i, filename in enumerate(image_filenames):
+        img_path = os.path.join(folder_path, filename)
+        img = Image.open(img_path)
+        
+        # แสดงรูปภาพในกริด
+        axes[i].imshow(img)
+        axes[i].set_title(filename)
+        axes[i].axis('off')
+
+    # ปรับลำดับของกริดและเฉพาะบรรทัดสุดท้ายหากไม่เต็มแถว
+    for i in range(num_images, num_rows * num_cols):
+        fig.delaxes(axes[i])
+
+    plt.tight_layout()
+    plt.show()
+
+
--- txt2img.py
+++ txt2img.py
@@ -1,8 +1,10 @@
 import torch
+import os
 from diffusers import StableDiffusionPipeline
 import argparse
 from IPython.display import Image
 from utils import save_img
+from image import display_images_sorted
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--prompt", type=str)
@@ -16,7 +18,7 @@ pipe.enable_xformers_memory_efficient_attention()
 prompt = args.prompt
 image = pipe(prompt).images[0]
 save = save_img(image, "outputs/txt2img")
-Image(save)
+display_images_sorted(os.path.dirname(save), num_cols=5, image_width=4)
 
 del pipe
 torch.cuda.empty_cache()
