b5f5104076ec3da80812fc40e841b2e7675aa208
Kimyobu <91616822+Kimyobu@users.noreply.github.com>
2023-09-21 12:56:33 +0700

    disable safety_check

--- txt2img.py
+++ txt2img.py
@@ -9,7 +9,7 @@ parser.add_argument("--prompt", type=str)
 
 args = parser.parse_args()
 
-pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, safety_check=None)
 pipe = pipe.to("cuda")
 pipe.enable_xformers_memory_efficient_attention()
 
