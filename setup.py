d4b88f85364abf5926f5cd3ac76a473b0525b4cf
Kimyobu <91616822+Kimyobu@users.noreply.github.com>
2023-09-21 11:32:49 +0700

    fix

--- setup.py
+++ setup.py
@@ -1,3 +1,69 @@
-from utils import install_req
+import argparse
+import subprocess
+import importlib
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("venv", type=str)
+
+args = parser.parse_args()
+
+venv = args.venv
+
+def run(cmd: str):
+    return subprocess.run(f"source {venv}; {cmd}", shell=True)
+
+def is_installed(name: str, pkg_version: str or None = None, operator: str = '=='):
+    out = False
+    try:
+        package = importlib.util.find_spec(name)
+        if package is not None:
+            out = True
+            if pkg_version is not None:
+                ver = version(package.name)
+                if operator == '==':
+                    out = (ver == pkg_version)
+                elif operator == '>=':
+                    out = (ver >= pkg_version)
+                elif operator == '<=':
+                    out = (ver <= pkg_version)
+                elif operator == '<':
+                    out = (ver < pkg_version)
+                elif operator == '>':
+                    out = (ver > pkg_version)
+                elif operator == '!=':
+                    out = (ver != pkg_version)
+                elif operator == '~=':
+                    out = (pkg_version in package.requires)
+    except ModuleNotFoundError:
+        pass
+    return out
+
+def run_pip(install_syntax: str):
+    operators = ['<', '>', '==', '>=', '<=' , '!=']
+    found = []
+
+    for op in operators:
+        if op in install_syntax:
+            found.append(op)
+
+    operator = found[-1] if len(found) > 0 else '=='
+
+    if operator is not None:
+        package_info = install_syntax.split(operator)
+        name = package_info[0].strip()
+        version = package_info[1].strip() if len(package_info) > 1 else None
+
+        if is_installed(name, version, operator) is False and name != '':
+            print(f'Install {install_syntax}')
+            run(f"pip install --no-cache-dir -q {install_syntax}")
+            
+def install_req(file):
+    op = open(file, 'r')
+    r = op.read()
+    op.close()
+    for x in r.split('\n'):
+        run_pip(x)
+
+
 
 install_req("requirements.txt")
--- utils.py
+++ utils.py
@@ -1,15 +1,8 @@
 import os
-import subprocess
-import argparse
 from datetime import datetime
 from PIL import Image
 
-parser = argparse.ArgumentParser(description="")
-parser.add_argument("venv", type=str)
 
-args = parser.parse_args()
-
-venv = args.venv
 
 def save_img(image, folder_path):
     # รับวันที่ปัจจุบัน
@@ -38,57 +31,3 @@ def save_img(image, folder_path):
 # ส่งรูปภาพและโฟลเดอร์ปลายทางไปยังฟังก์ชัน
 # save_image_with_date_and_sequence(image, "/path/to/your/folder")
 
-def run(cmd: str):
-    return subprocess.run(f"source {venv}; {cmd}", shell=True)
-
-def is_installed(name: str, pkg_version: str or None = None, operator: str = '=='):
-    out = False
-    try:
-        package = importlib.util.find_spec(name)
-        if package is not None:
-            out = True
-            if pkg_version is not None:
-                ver = version(package.name)
-                if operator == '==':
-                    out = (ver == pkg_version)
-                elif operator == '>=':
-                    out = (ver >= pkg_version)
-                elif operator == '<=':
-                    out = (ver <= pkg_version)
-                elif operator == '<':
-                    out = (ver < pkg_version)
-                elif operator == '>':
-                    out = (ver > pkg_version)
-                elif operator == '!=':
-                    out = (ver != pkg_version)
-                elif operator == '~=':
-                    out = (pkg_version in package.requires)
-    except ModuleNotFoundError:
-        pass
-    return out
-
-def run_pip(install_syntax: str):
-    operators = ['<', '>', '==', '>=', '<=' , '!=']
-    found = []
-
-    for op in operators:
-        if op in install_syntax:
-            found.append(op)
-
-    operator = found[-1] if len(found) > 0 else '=='
-
-    if operator is not None:
-        package_info = install_syntax.split(operator)
-        name = package_info[0].strip()
-        version = package_info[1].strip() if len(package_info) > 1 else None
-
-        if is_installed(name, version, operator) is False and name != '':
-            print(f'Install {install_syntax}')
-            run(f"pip install --no-cache-dir -q {install_syntax}")
-            
-def install_req(file):
-    op = open(file, 'r')
-    r = op.read()
-    op.close()
-    for x in r.split('\n'):
-        run_pip(x)
